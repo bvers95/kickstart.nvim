f473692d610c016aff41d4cd9d94221bd98bbcdf
Boris Versteegh <boris.versteegh@devoteam.com>
2025-11-23 12:20:27 +0100

    Enabled Pyright LSP

--- init.lua
+++ init.lua
@@ -664,7 +664,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
